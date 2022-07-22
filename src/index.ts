53cc7ddbd8451bb74f197a0eaa7129ee248385cc
unverzed <vitoriarortega@gmail.com>
2022-07-22 13:55:29 -0300

    feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express()
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => console.log(`Server working on port ${process.env.PORT}`));
